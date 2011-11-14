a475eb6763334e82021e4e38356d8e4f3b4a5ea1
fengmk2 <fengmk2@gmail.com>
2011-11-15 01:37:19 +0800

    add delete demo

--- test/metaweblog.test.js
+++ test/metaweblog.test.js
@@ -6,6 +6,10 @@ var MetaWeblog = require('../lib/metaweblog').MetaWeblog;
 
 var blog = new MetaWeblog('http://www.cnblogs.com/fengmk2/services/metaweblog.aspx');
 
-blog.getUsersBlogs('fawave', 'fengmk2', 'xxx', function(err, data) {
-	console.log(err, data);
+//blog.getUsersBlogs('fawave', 'fengmk2', 'xxx', function(err, data) {
+//	console.log(err, data);
+//});
+
+blog.deletePost('fawave', '2249012', 'fengmk2', 'xxx', true, function(err, data) {
+    console.log(err, data);
 });
